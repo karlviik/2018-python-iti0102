5aaec61c5829a6cfbd307e6cf29941535e8ab0d2
karlviik <viik.karl2@gmail.com>
2018-09-11 11:12:21 +0300

    janguru version 4

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -11,7 +11,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
         j1, j2 = j2, j1
         p1, p2 = p2, p1
 
-    if ((((y2-y1)>j1)or(j1<j2))and(j1/p1<j2/p2)) or ((y2-y1)%j1!=0 and j2%j1==0) or (j1==j2 and p1==p2 and y1!=y2):
+    if ((((y2-y1) > j1)or(j1 < j2))and(j1/p1 < j2/p2)) or ((y2-y1) % j1 != 0 and j2 % j1 == 0) or (j1 == j2 and p1 == p2 and y1 != y2):
         return -1
     else:
         littletemp = j2 * p1 - j1 * p2
@@ -19,10 +19,8 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
         i = 0
         j = 0
         minmeet = float("inf")
-        tempmeet = 0
-        while i < p1:
-            stepper = 1
-            while j < p2:
+        while i <= p1:
+            while j <= p2:
                 if (largetemp + j1 * j2 * (j-i)) % littletemp == 0:
                     tempmeet = ((largetemp + j1 * j2 * (j-i)) / littletemp)
                     if tempmeet % 1 == 0 and tempmeet < minmeet and tempmeet >= 0 and tempmeet > y1 and tempmeet > y2 and (tempmeet - y1)%j1==0 and (tempmeet - y2)%j2==0:
@@ -31,7 +29,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
                     tempmeet = ((largetemp + j1 * j2 * (i-j)) / littletemp)
                     if tempmeet % 1 == 0 and tempmeet < minmeet and tempmeet >= 0 and tempmeet > y1 and tempmeet > y2 and (tempmeet - y1)%j1==0 and (tempmeet - y2)%j2==0:
                         minmeet = tempmeet
-                j += stepper
+                j += 1
             i += 1
         if minmeet == float("inf"):
             return -1
