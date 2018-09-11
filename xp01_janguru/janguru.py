37d7604c7b024375dd2c0a6498c3181e7150af93
karlviik <viik.karl2@gmail.com>
2018-09-11 11:23:30 +0300

    janguru version 8

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -11,7 +11,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
         j1, j2 = j2, j1
         p1, p2 = p2, p1
 
-    if ((((y2-y1) > j1) or (j1 < j2)) and (j1/p1 < j2/p2)) or ((y2-y1) % j1 != 0 and j2 % j1 == 0) or (j1 == j2 and p1 == p2 and y1 != y2):
+    if ((((y2 - y1) > j1) or (j1 < j2)) and (j1 / p1 < j2 / p2)) or ((y2 - y1) % j1 != 0 and j2 % j1 == 0) or (j1 == j2 and p1 == p2 and y1 != y2):
         return -1
     else:
         littletemp = j2 * p1 - j1 * p2
@@ -25,7 +25,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
             while j < p2:
                 if (largetemp + j1 * j2 * (j - i)) % littletemp == 0:
                     tempmeet = ((largetemp + j1 * j2 * (j - i)) / littletemp)
-                    if tempmeet % 1 == 0 and tempmeet < minmeet and tempmeet >= 0 and tempmeet > y1 and tempmeet > y2 and (tempmeet - y1) % j1 ==  0 and (tempmeet - y2) % j2 == 0:
+                    if tempmeet % 1 == 0 and tempmeet < minmeet and tempmeet >= 0 and tempmeet > y1 and tempmeet > y2 and (tempmeet - y1) % j1 == 0 and (tempmeet - y2) % j2 == 0:
                         minmeet = tempmeet
                 if (largetemp + j1 * j2 * (i - j)) % littletemp == 0:
                     tempmeet = ((largetemp + j1 * j2 * (i - j)) / littletemp)
