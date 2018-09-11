11520b66b4587629bef91070c7c3f1604d320dd6
karlviik <viik.karl2@gmail.com>
2018-09-11 12:55:21 +0300

    pr02 v6

--- pr02_circle/pr02_circle.py
+++ pr02_circle/pr02_circle.py
@@ -14,7 +14,7 @@ def find_circle_info(d, x, y):
         place = "outside"
     perimeter = 2 * math.pi * r
     area = math.pi * r ** 2
-    print(f"Circle with perimeter of {perimeter} units and area of {area} units has point ({x}, {y}) on its {place}.")
+    print(f"Circle with perimeter of {perimeter} units and area of {area} units has point ({x}, {y}) on its {place}")
 
 
 if __name__ == "__main__":  # <- This line is needed for automatic testing
