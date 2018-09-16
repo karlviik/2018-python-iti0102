702441e3c415269a57ddcc32fb3a1de4745c7ea4
karlviik <viik.karl2@gmail.com>
2018-09-16 22:16:46 +0300

    janguru new formula v4

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -8,17 +8,17 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     divider = (jump_distance1 * sleep2 - jump_distance2 * sleep1)  # variable for cases where divider == 0
     if divider == 0:  # changes variable to 1 to be able to get correct answer in these cases
         divider = 1
-    t1 = (sleep1 * sleep2 * (pos2 - pos1 - jump_distance1)) / divider  # meeting point for 2 linear functions
-    t2 = (sleep1 * sleep2 * (pos2 + jump_distance2 - pos1)) / divider  # other point
-    if t1 < 0:  # these are for cases where point is negative
-        t1 = 0
-    if t2 < 0:
-        t2 = 0
-    if t2 < t1:  # orders them so that t1 is lower than t2
-        t1, t2 = t2, t1
-    current_time1 = t1 - t1 % sleep1  # gets lowest time in that window
-    current_time2 = t1 - t1 % sleep2
-    while current_time1 <= t2 or current_time2 <= t2:  # until either of the times is still in window
+    time1 = (sleep1 * sleep2 * (pos2 - pos1 - jump_distance1)) / divider  # meeting point for 2 linear functions
+    time2 = (sleep1 * sleep2 * (pos2 + jump_distance2 - pos1)) / divider  # other point
+    if time1 < 0:  # these are for cases where point is negative
+        time1 = 0
+    if time2 < 0:
+        time2 = 0
+    if time2 < time1:  # orders them so that time1 is lower than time2
+        time1, time2 = time2, time1
+    current_time1 = time1 - time1 % sleep1  # gets lowest time in that window
+    current_time2 = time1 - time1 % sleep2
+    while current_time1 <= time2 or current_time2 <= time2:  # until either of the times is still in window
         if current_time1 < current_time2:
             tempmeet = current_time1 / sleep1 * jump_distance1 + jump_distance1 + pos1  # gets potential meet position
             if (current_time1 - current_time1 % sleep2)/sleep2 * jump_distance2 + jump_distance2 + pos2 == tempmeet:  # checks if it's the meet position
