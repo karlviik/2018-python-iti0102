a42e7672ca1226c0f0f514025dff11b31630c73a
karlviik <viik.karl2@gmail.com>
2018-10-01 13:35:36 +0300

    xp02 v2

--- xp02_formula/formula.py
+++ xp02_formula/formula.py
@@ -1,15 +1,15 @@
 import re
 
 regex_a = "(([+-]\s)?\d*)x2"
-regex_b = "(([+-]\s)?\d*)x[^2-9]"
+regex_b = "(([+-]\s)?\d*)x([^2-9]|$)"
 regex_c = "(?<!x2)([+-]\s)?\d+(?=[=\s$])"
-regex_c = "(?<![x(x2)(x1)])(([+-]\s)?\d+(?!x))"
+regex_c = "(?<![x(x2)(x1)])(([+-]\s)?\d+((?!x)|$))"
 
 if __name__ == '__main__':
 
     def print_regex_results(regex, f):
         for match in re.finditer(regex, f):
-            if match != "":
+            if match != "aaaaaaaaa":
                 print(match.group(1))
 
 
@@ -20,13 +20,17 @@ if __name__ == '__main__':
     #print_regex_results(regex_b, f)  # - 4
     #print_regex_results(regex_c, f)  # 1
 
-    f2 = "3x2 + 4x + 5 - 2x2 - 7x + 4"
+    #f2 = "3x2 + 4x + 5 - 2x2 - 7x + 4"
 
-    print("x2")
-    print_regex_results(regex_a, f2)  # 3, - 2
-    print("x")
-    print_regex_results(regex_b, f2)  # 4, - 7
-    print("c")
-    print_regex_results(regex_c, f2)  # 5, 4
+    #print("x2")
+    #print_regex_results(regex_a, f2)  # 3, - 2
+    #print("x")
+    #print_regex_results(regex_b, f2)  # 4, - 7
+    #print("c")
+    #print_regex_results(regex_c, f2)  # 5, 4
+
+
+    f = "3x"
+    print_regex_results(regex_b, f)
 
     print("end")
