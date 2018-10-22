77c10374a8ff4735f485ddab4e8409e4d0f108a9
karlviik <viik.karl2@gmail.com>
2018-10-22 09:45:29 +0300

    xp03 v1.3 hunting season

--- xp03_heatmap/heatmapper.py
+++ xp03_heatmap/heatmapper.py
@@ -123,7 +123,10 @@ def generate_map_with_coordinates(topo_params, image_width, image_height, filena
         if lonstep == 0:
             lonstep = 1
     file = f"topo_{topo_params[0]}-{topo_params[1]}-{latstep}_{topo_params[3]}-{topo_params[4]}-{lonstep}.json"
-    contents = topo.read_json_from_file(file)
+    try:
+        contents = topo.read_json_from_file(file)
+    except:
+        return False
     if not contents:
         contents = topo.read_json_from_web(topo_params[0], topo_params[1], topo_params[2], topo_params[3], topo_params[4], topo_params[5])
         with open(file, "w") as f:
