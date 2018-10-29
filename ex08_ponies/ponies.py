6a3f94f07c732c73dfeddf300de0fc146938ee07
karlviik <viik.karl2@gmail.com>
2018-10-29 19:19:26 +0200

    ponies

--- ex08_ponies/ponies.py
+++ ex08_ponies/ponies.py
@@ -16,7 +16,12 @@ def decode(line: str) -> str:
 
 
 def extract_information(line: str) -> dict:
+    """
+    Extract pony info and put into dictionary.
 
+    :param line: string of pony attributes
+    :return: dictionary of pony attributes
+    """
     regex = "[^(\\s{2,'inf'})]([a-zA-Z]|(\\s(?!\\s)))+"
     info_type_list = ["name", "kind", "coat_color", "mane_color", "eye_color", "location"]
     info_dict = {}
@@ -29,35 +34,134 @@ def extract_information(line: str) -> dict:
 
 
 def read(read_file: str) -> list:
-    pass
+    """
+    Read coded lines from file, decode, extract info and put dictionaries into list.
+
+    :param read_file: file with coded lines
+    :return: list of dictionaries of pony info
+    """
+    try:
+        with open(read_file, "r") as f:
+            lines = f.readlines()
+    except FileNotFoundError:
+        raise Exception("File not found!")
+    ponylist = []
+    for count, line in enumerate(lines):
+        if count > 1:
+            ponylist.append(extract_information(decode(line)))
+    return ponylist
 
 
 def filter_by_location(ponies: list) -> list:
-    pass
+    """
+    Filter out ponies with location set as none.
+
+    :param ponies: list of pony dictionaries
+    :return: list of filtered pony dictionaries
+    """
+    filteredponies = []
+    for counter, ponydict in enumerate(ponies):
+        if ponydict["location"] != "None":
+            filteredponies.append(ponydict)
+    return filteredponies
 
 
 def filter_by_kind(ponies: list, kind: str) -> list:
-    pass
+    """
+    Return dict of ponies with kind set as input string kind.
+
+    :param ponies: pony info dictionary
+    :param kind: required kind
+    :return: list of pony dictionaries with kind set as input
+    """
+    filteredponies = []
+    for counter, ponydict in enumerate(ponies):
+        if ponydict["kind"] == kind:
+            filteredponies.append(ponydict)
+    return filteredponies
 
 
-def get_points_for_color(color: str) -> int:
-    pass
+def get_points_for_color(color: str) -> int or None:
+    """
+    Return amount of points for that color.
+
+    :param color: color to be evaluated
+    :return: points for said color based on list
+    """
+    colors = [
+        'magenta', 'pink', 'purple', 'orange', 'red', 'yellow', 'cyan', 'blue', 'brown', 'green'
+    ]
+    for counter, listcolor in enumerate(colors):
+        if listcolor == color:
+            points = abs(counter - 10)
+            if points < 5:
+                return None
+            return points
 
 
 def add_points(pony: dict) -> dict:
-    pass
+    """
+    Add key "points" to the dict and return it.
+
+    :param pony: dict of pony info
+    :return: dict of pony info plus points key
+    """
+    pony["points"] = None
+    return pony
 
 
 def evaluate_ponies(ponies: list) -> list:
-    pass
+    """
+    Give points for pony colours and add it to key "points" and return new list with new dicts.
+
+    :param ponies: list of pony dicts
+    :return: list of pony dicts with new points key
+    """
+    evaluation_locations = {
+        'coat_color': ['Town Hall', 'Theater', 'School of Friendship'],
+        'mane_color': ['Schoolhouse', 'Crusaders Clubhouse', 'Golden Oak Library'],
+        'eye_color': ['Train station', 'Castle of Friendship', 'Retirement Village']
+    }
+    newponies = []
+    for pony in ponies:
+        pony = add_points(pony)
+        location = pony["location"]
+        for key, locations in evaluation_locations.items():
+            if location in locations:
+                colortograde = pony[key]
+                break
+        pony["points"] = get_points_for_color(colortograde)
+        newponies.append(pony)
+    return newponies
 
 
 def sort_by_name(ponies: list) -> list:
-    pass
+    """
+    Sort the list of dictionaries in alphabetical order based on pony names.
+
+    :param ponies:
+    :return:
+    """
+    def get_name(dictionary):
+        return dictionary["name"]
+    return sorted(ponies, key=get_name)
 
 
 def sort_by_points(ponies: list) -> list:
-    pass
+    """
+    Sort the list of dictionaries based on pony points.
+
+    :param ponies: list of pony dicts
+    :return: list of sorted pony dicts by points
+    """
+    newponies = []
+    for pony in ponies:
+        if pony["points"] != None:
+            newponies.append(pony)
+
+    def get_points(dictionary):
+        return dictionary["points"]
+    return sorted(newponies, key=get_points, reverse=True)
 
 
 def format_line(pony: dict, place: int) -> str:
@@ -68,5 +172,5 @@ def write(input_file: str, kind: str):
     pass
 
 
-assert extract_information(' MaudPommel       Unicorn       pink  green     cyan   Castle of Friendship\n') == {'name': 'MaudPommel', 'kind': 'Unicorn', 'coat_color': 'pink', 'mane_color': 'green', 'eye_color': 'cyan', 'location': 'Castle of Friendship'}
 
+print(decode("TkFNRSAgICAgICAgICAgICAgICBLSU5EICAgICAgICAgICAgICAgIENPQVQgQ09MT1IgICAgICAgICAgTUFORSBDT0xPUiAgICAgICAgICBFWUUgQ09MT1IgICAgICAgICAgIExPQ0FUSU9O"))
