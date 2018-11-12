d40b8d6f3489cab381f8ae5c952cc3b555a1eb60
karlviik <viik.karl2@gmail.com>
2018-11-12 22:35:49 +0200

    O1

--- wat/wat.py
+++ wat/wat.py
@@ -1,26 +1,14 @@
 """Do wat."""
-import random
-A = [0, 2, 4, 6, 8, 11, 12, 14, 16, 18]
-B = 0
+FIRST_LIST = [0, 2, 4, 6, 8, 11, 12, 14, 16, 18]
+FIRST_COUNTER = 0
+
 
 def first(n: int):
     """Do wat."""
-    global B
-    B += 1
-    return A[B - 1]
-    if 8 < n < 1000:
-        return random.choice([8, 12])
-    if n > 1000:
-        return 18
-    if n == 0:
-        return 16
-    if n == 1:
-        return 0
-    if n == 2:
-        return 2
-    if 2 < n < 9:
-        return random.choice([4, 6])
-    if n == -1:
-        return 14
-    else:
-        return random.choice([11, 12])
+    global FIRST_COUNTER
+    FIRST_COUNTER += 1
+    return FIRST_LIST[FIRST_COUNTER - 1]
+
+
+def last(n: int):
+    pass
