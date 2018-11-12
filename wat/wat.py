fec408b961f5d5f40bd7fcf5ce25f2872fd77e1e
karlviik <viik.karl2@gmail.com>
2018-11-12 22:12:18 +0200

    O1

--- wat/wat.py
+++ wat/wat.py
@@ -2,4 +2,17 @@
 import random
 
 def first(n: int):
-    return random.choice([0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
+    if 8 < n < 1000:
+        return random.choice([8, 12])
+    if n > 1000:
+        return 18
+    if n == 0:
+        return 16
+    if n == 1:
+        return 0
+    if n == 2:
+        return 2
+    if 2 < n < 9:
+        return random.choice([4, 6])
+    else:
+        return random.choice([11, 12])
