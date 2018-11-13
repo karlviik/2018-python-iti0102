d5f40c73547b86e5b355424288cf136dc7eb6bf9
karlviik <viik.karl2@gmail.com>
2018-11-13 14:09:34 +0200

    wat

--- wat/wat.py
+++ wat/wat.py
@@ -13,35 +13,43 @@ def first(n: int):
 
 def last_neg(n):
     """Negative wat."""
-    if n > -15:
+    if -12000 <= n <= -7020:
+        return int(-1000 + abs(12000 + n) / 3)
+    if -7019 <= n <= -1624:
+        pass  # aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    if -1623 <= n <= -625:
+        return -1345 + 2 * abs(1623 + n)
+    if -624 <= n <= -15:
+        return -11355 + 19 * abs(624 + n)
+    if -14 <= n <= -4:
         return 0
-    if n > -625:
-        pass
-    if n > -1624:
-        pass
-    if n > -7020:
-        pass
 
 
 def last_pos(n):
     """Positive wat."""
-    if n < 103:
-        pass
-    elif n < 999:
-        pass
-    elif n < 1012:
-        pass
-    elif n < 2003:
-        pass
-    elif n < 7982:
-        pass
-    else:
-        pass
+    if -3 <= n <= 102:
+        return 2 * (n + 3)
+    if 103 <= n <= 998:
+        return int(-616 + (n - 103) * 1.5 - (int(n / 138)) * 137)
+    if 999 <= n <= 1011:
+        return chr((n - 999) * 2 + 98)
+    if 1012 <= n <= 2002:
+        return 33 + int((n - 1096) / 84)  # aaaaaaaaaaaaaaaaaaaaaaaaaaa
+    if 2003 <= n <= 7981:
+        return n - int(n / 1337) * 1337
+    if 7982 <= n <= 12000:
+        n = str(n)
+        nsum = 0
+        for num in n:
+            nsum += int(num)
+        return nsum + 1
 
 
 def last(n: int):
     """Last wat."""
-    if n >= -3:
-        return last_pos(n)
-    else:
+    if n < -3:
         return last_neg(n)
+    else:
+        return last_pos(n)
+
+print(last(-9350))
