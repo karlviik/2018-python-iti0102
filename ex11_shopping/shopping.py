6e81503587438d55d7c446bc82b8eea4de9f05bd
karlviik <viik.karl2@gmail.com>
2018-11-19 13:53:01 +0200

    Ex11

--- ex11_shopping/shopping.py
+++ ex11_shopping/shopping.py
@@ -1,6 +1,10 @@
 """Store imitation."""
 
-# TODO: create class for custom exception.
+
+class ProductCannotBeSold(Exception):
+    """Exception raised when it's not allowed or possible to make the sale."""
+
+    pass
 
 
 class Product:
@@ -13,7 +17,8 @@ class Product:
         :param name: product name
         :param price: product price
         """
-        pass
+        self.name = name
+        self.price = price
 
     def __str__(self) -> str:
         """
@@ -21,7 +26,7 @@ class Product:
 
         :return: string
         """
-        pass
+        return f"Product: {self.name}, price: {self.price}"
 
     def __repr__(self) -> str:
         """
@@ -29,7 +34,7 @@ class Product:
 
         :return: string
         """
-        pass
+        return self.name
 
 
 class Customer:
@@ -44,7 +49,10 @@ class Customer:
         :param age: customer's age
         :param money: customer's money
         """
-        pass
+        self.name = name
+        self.age = age
+        self.money = money
+        self.items = {}
 
     def add_item(self, product: Product, amount: int) -> None:
         """
@@ -53,17 +61,23 @@ class Customer:
         :param product: product
         :param amount: amount
         """
-        pass
+        if product.name not in self.items.keys():
+            self.items[product.name] = amount
+        else:
+            self.items[product.name] += amount
 
     def pay(self, money_to_pay: int) -> None:
         """
-        Checks if customer has enough money to pay for the product.
+        Check if customer has enough money to pay for the product.
 
         Returns nothing, but raises exception if customer has not enough money to pay.
         In other case reduces amount of customer's money.
         :param money_to_pay: money amount needed to be paid
         """
-        pass
+        if money_to_pay > self.money:
+            raise ProductCannotBeSold("You do not have enough money to pay for chosen product!")
+        else:
+            self.money -= money_to_pay
 
     def __str__(self) -> str:
         """
@@ -71,7 +85,16 @@ class Customer:
 
         :return: string
         """
-        pass
+        itemlist = []
+        for item, amount in self.items.items():
+            if amount > 1:
+                item += f"({amount})"
+            itemlist.append(item)
+        if len(itemlist) > 1:
+            stringitemlist = ", ".join(itemlist)
+        else:
+            stringitemlist = itemlist[0]
+        return f"{self.name}'s items: {stringitemlist}; money: {self.money}."
 
 
 class Store:
@@ -79,41 +102,55 @@ class Store:
 
     def __init__(self) -> None:
         """Class constructor."""
-        pass
+        self.money = 0
+        self.products = {}
 
     def buy(self, product: Product, amount: int, customer: Customer) -> str:
         """
-        Represents how customer buys product.
+        Represent how customer buys product.
 
         :param product: product the customer wants
         :param amount: pieces of product
         :param customer: customer who wants to buy
         :return: message
         """
-        pass
+        try:
+            self.check_product_availability(product, amount)
+            self.allowed_to_buy(product, customer)
+            customer.pay(amount * self.products[product.name][0])
+            customer.add_item(product, amount)
+            self.products[product.name][1] -= 1
+            self.money += self.products[product.name][0]
+            return "Thank you for the purchase!"
+        except ProductCannotBeSold as errormessage:
+            return errormessage
 
     def allowed_to_buy(self, product: Product, customer: Customer):
         """
-        Checks if customer is allowed to buy some particular products.
+        Check if customer is allowed to buy some particular products.
 
         Permission depends on customer's age
 
-        Customers under 18 are not allowed to buy alcohol and tobacco.
+        Customers under 18 are not allowed to buy beer and tobacco.
         Must raise exception if customer has no permission to buy chosen product.
         :param product: product to buy
         :param customer: customer who makes the purchase
         """
-        pass
+        if customer.age < 18 and (product.name == "beer" or product.name == "tobacco"):
+            raise ProductCannotBeSold(f"You are too young to buy {product.name}!")
 
     def check_product_availability(self, product: Product, amount: int):
         """
-        Checks if chosen amount of product is present in stock.
+        Check if chosen amount of product is present in stock.
 
         Must raise exception if no product found or not enough in stock.
         :param product: product to be bought
         :param amount: amount of product
         """
-        pass
+        if product.name not in self.products.keys():
+            raise ProductCannotBeSold("Item not found!")
+        elif self.products[product.name][1] < amount:
+            raise ProductCannotBeSold("Item is not available in chosen amount!")
 
     def add_product(self, product: Product) -> None:
         """
@@ -121,15 +158,25 @@ class Store:
 
         :param product:  product name
         """
-        pass
+        if product.name not in self.products.keys():
+            self.products[product.name] = [product.price, 1]
+        else:
+            self.products[product.name][1] += 1
 
     def __str__(self) -> str:
         """
-        Store's object representation in string format.
+        Store object representation in string format.
 
         :return: string
         """
-        pass
+        itemlist = []
+        for item, price_and_amount in self.products.items():
+            if price_and_amount[1] > 1:
+                item += f"({price_and_amount[1]})"
+            if price_and_amount[1]:
+                itemlist.append(item)
+        stringitemlist = ", ".join(itemlist)
+        return f"Store items: {stringitemlist}; store money: {self.money}."
 
 
 if __name__ == "__main__":
@@ -156,7 +203,7 @@ if __name__ == "__main__":
 
     tobacco = Product("tobacco", 55)
     store.add_product(tobacco)
-    print(store.buy(tobacco, 1, bobby))  # -> You are too young to buy Product: tobacco, price: 55!
+    print(store.buy(tobacco, 1, bobby))  # -> You are too young to buy Product: tobacco!
 
     print(store.buy(water, 2, sandy))  # -> Item is not available in chosen amount!
 
@@ -164,8 +211,8 @@ if __name__ == "__main__":
     print(store.buy(candy, 1, bobby))  # -> Item not found!
 
     store.buy(choco, 2, bobby)
-    print(store.products.count(choco))  # -> 1
     print(bobby.money)  # -> 60
     store.buy(water, 1, bobby)
-    print(bobby)  # -> Bobby's items: chocolate(2), water; money: 30
+    print(bobby)  # -> Bobby's items: chocolate(2), water; money: 30.
+
 
