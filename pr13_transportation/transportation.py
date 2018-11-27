65141932faa764278b37e428db3cb693cc71d7ad
karlviik <viik.karl2@gmail.com>
2018-11-27 10:01:29 +0200

    pr13

---
+++ pr13_transportation/transportation.py
@@ -0,0 +1,76 @@
+"""Retrieve stops and departures info from REST service."""
+import requests
+
+API_BASE = "https://public-transport-api.herokuapp.com"
+REGION = "tallinn"
+
+
+def get_nearby_stops(api_base, lat, lng):
+    """
+    Get nearby stops.
+
+    :param api_base: Base URL that the endpoint gets appended to
+    :param lat: Latitude
+    :param lng: Longitude
+    :return: List of all nearby stops
+    """
+    response = requests.get(f"{api_base}/stops/{lat}/{lng}").json()
+    if response != []:
+        response = sorted(response, key=(lambda x: x["distance"]))
+    return response
+
+
+def get_nearest_stop(api_base, lat, lng):
+    """
+    Get nearest stop.
+
+    :param api_base: Base URL that the endpoint gets appended to
+    :param lat: Latitude
+    :param lng: Longitude
+    :return: Nearest stop
+    """
+    response = get_nearby_stops(api_base, lat, lng)
+    if response != []:
+        response = response[0]
+    else:
+        response = None
+    return response
+
+
+def get_next_departures(api_base, region, stop_id):
+    """
+    Get next departures from stop.
+
+    :param api_base: Base URL that the endpoint gets appended to
+    :param region: Region
+    :param stop_id: Stop ID
+    :return: List of next departures from stop
+    """
+    response = requests.get(f"{api_base}/departures/{region}/{stop_id}").json()
+    if response != []:
+        response = response["departures"]
+    return response
+
+
+def get_next_departure(api_base, region, stop_id):
+    """
+    Get next departure from stop.
+
+    :param api_base: Base URL that the endpoint gets appended to
+    :param region: Region
+    :param stop_id: Stop ID
+    :return: Next departure from stop
+    """
+    response = get_next_departures(api_base, region, stop_id)
+    if response != []:
+        response = response[0]
+    else:
+        response = None
+    return response
+
+
+if __name__ == '__main__':
+    print(get_nearby_stops(API_BASE, 59.3977111, 24.660198))
+    print(get_nearest_stop(API_BASE, 59.3977111, 24.660198))
+    print(get_next_departures(API_BASE, REGION, get_nearest_stop(API_BASE, 59.3977111, 24.660198)["id"]))
+    print(get_next_departure(API_BASE, REGION, get_nearest_stop(API_BASE, 59.3977111, 24.660198)["id"]))
