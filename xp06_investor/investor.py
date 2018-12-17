a0d2a0b17ec6a8b3ad220177c74bd7fe9f6ae240
karlviik <viik.karl2@gmail.com>
2018-12-17 22:55:46 +0200

    Investor s1ck autotester test because I'm afraid of testing this thing myself because it's just so wicked and yeah, so that's that.

--- xp06_investor/investor.py
+++ xp06_investor/investor.py
@@ -1,5 +1,6 @@
 """Get a decent investing plan based on math and stuff."""
 import csv
+import datetime
 
 
 def get_currency_rates_from_file(filename: str) -> tuple:
@@ -17,6 +18,7 @@ def get_currency_rates_from_file(filename: str) -> tuple:
     :param filename: file name to read CSV data from
     :return: Tuple that consists of currency name and dict with exchange rate history
     """
+    rates = {}
     # Opens the file.
     with open(filename) as csv_file:
         # Saves the file as a csv.reader object and separates the lines in file to lists of strings which were separated by the delimiter.
@@ -28,13 +30,9 @@ def get_currency_rates_from_file(filename: str) -> tuple:
             if row[0] == "":
                 currency = row[1]
             elif row[1] != "":
-
-
-    return ("USD", {
-        "01.01.2017": 1.0234,
-        "02.01.2017": 1.0101,
-        "03.01.2017": 1.0099
-    })
+                rates[row[0]] = row[1]
+    print(rates)
+    return currency, rates
 
 
 def exchange_money(exchange_rates: dict) -> list:
@@ -55,9 +53,90 @@ def exchange_money(exchange_rates: dict) -> list:
     :param exchange_rates: dictionary of dates and exchange rates
     :return: list of dates
     """
-    return [
-        "01.01.2017",
-        "03.01.2017"
-    ]
+    def actualscanner(index):
+        while True:
+            if dateprices[index][1]:
+                top = dateprices[index][0]
+                break
+            index += 1
+        do_i_have_profit = False
+        pricelist = []
+        index += 1
+        indexlimit = len(dateprices)
+        while True:
+            newprice = dateprices[index]
+            if not do_i_have_profit:  # if  yet do not have a profitable bottom
+                if newprice[1] and newprice[0][1] > top[1]:  # if I've gotten a top that is higher than current top
+                    return index, pricelist  # return the index and pricelist
+                elif not newprice[1]:  # if I've gotten a bottom
+                    if newprice[0][1] < 0.9801 * top[1]:  # if the bottom is profitable
+                        do_i_have_profit = True  # change the flag to true
+                        pricelist = [top, newprice[0]]  # and add the top and profitable bottom pair to pricelist
+                    elif not pricelist:  # if the list is empty and I got an unprofitable bottom
+                        pricelist = [newprice[0]]  # add it to the list alone
+                    elif pricelist[0][1] < newprice[0][1]:  # if current solo unprofitable bottom is higher than scanned unprofitable bottom
+                        pricelist = [newprice[0]]  # overwrite the other one (could have combined, but clarity)
+            else:  # if I do have a pair in pricelist
+                if newprice[1]:  # if dealing with a top
+                    if newprice[0][1] > top[1]:  # if scanned top higher than current top:
+                        return index, pricelist
+                    elif  newprice[0][1] > pricelist[-1][1] / 0.99:  # if scanned top has potential to be profitable:
+                        index, newprices = actualscanner(index)
+                        index -= 1  # to counteract the +1 at the end of loop
+                        if len(newprices) == 1:  # if returned newprice list only has a bottom:
+                            if newprices[0][1] < pricelist[-1][1]:  # if that bottom is lower than current bottom
+                                pricelist[1] = newprices[0]  # replace it
+                        pricelist += newprices
+                else:  # if dealing with a bottom
+                    if pricelist[-1][1] > newprice[0][1]:  # if scanned bottom is lower than last bottom saved
+                        pricelist[-1] = newprice[0]  # replace it
+            index += 1
+            if index >= indexlimit:
+                break
+        return index, pricelist
 
-print(get_currency_rates_from_file("currency-rates.csv"))
+    exchange_rates["31.12.9999"] = float("inf")
+    goingup = True
+    last = [[0, 0], 0]
+    maxprice = 0
+    minprice = float("inf")
+    dateprices = []
+    for date, price in sorted(exchange_rates.items(), key=lambda x: datetime.datetime.strptime(x[0], "%d.%m.%Y").date()):
+
+        price = float(price)
+        if goingup:
+            if last[0][1] > price:
+                dateprices.append(last)
+                goingup = False
+                if maxprice < last[0][1]:
+                    maxprice = last[0][1]
+        else:
+            if last[1] < price:
+                dateprices.append(last)
+                goingup = True
+                if minprice > last[0][1]:
+                    minprice = last[0][1]
+        last = [[date, price], goingup]
+
+    print(maxprice, minprice)
+    if maxprice / minprice * 0.99 ** 2 <= 1:
+        return []
+
+    index = 0
+    endlist = []
+    while True:
+        index, lists = actualscanner(index)
+        if len(lists) > 1:
+            endlist += lists
+        print(endlist)
+        if index >= len(dateprices):
+            break
+
+    dates = []
+    for sublist in endlist:
+        dates.append(sublist[0])
+    return dates
+
+
+a, b = get_currency_rates_from_file("currency-rates.csv")
+print(exchange_money(b))
