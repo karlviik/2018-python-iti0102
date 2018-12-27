230273db9ca0b5f795a1a9031ec5ae34e70301aa
karlviik <viik.karl2@gmail.com>
2018-12-27 17:51:41 +0200

    Cleanup

--- dj01_django_intro/polls/admin.py
+++ dj01_django_intro/polls/admin.py
@@ -1,14 +1,17 @@
+"""Define how admin page looks and what functions it serves."""
 from django.contrib import admin
 
 from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
+    """Give input locations for choices."""
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    """Handle how question input looks."""
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
